b219818153e4d2a646692514fb89f8b92fd467d9
AlexPolubiakin <31985883+AlexPolubiakin@users.noreply.github.com>
2018-05-27 15:10:19 +0300

    Add files via upload

--- Chart.php
+++ Chart.php
@@ -1,11 +1,32 @@
 <?php
+/**
+ * @class Chart результатом исполнения является диаграмма на основе введенных данных.
+ */
 class Chart {
+    /**
+     * @param int $x - размер изображения по оси Х
+     * @param int $y - размер изображения по оси Y
+     * @return возвращает идентификатор изображения заданного размера как функция imagecreatetruecolor() 
+     */
     public function drawBg($x,$y){
         return imageCreateTrueColor($x,$y);   
     }
+
+    /**
+     * 
+     * @param ? $img - использует изображение, которое вернул метод drawBg();
+     * @param int $color - цвет
+     * @param int $startRad - градус начала дуги
+     * @param int $endRad - градус конца дуги
+     */
     public function drawArc($img,$color,$startRad,$endRad){
         imagefilledarc($img,150,150,280,280,$startRad,$endRad,$color,IMG_ARC_PIE);
     }
+    /**
+     * @param ? $img - использует изображение, которое вернул метод drawBg();
+     * @param array $dataArr - массив данных "элементы массива сторого int"
+     * @param array $colorArr - массив цветов сформированных функцией imagecolorallocate();
+     */
     public function drawDiagramm($img,$dataArr,$colorArr){
         $runs   = count($dataArr);
         $sum    = array_sum($dataArr);
@@ -25,7 +46,10 @@ class Chart {
             $colorwhite = imagecolorallocate($img, 0xFF, 0xFF, 0xFF);
             imagefilledarc($img,150,150,120,120,0,360,$colorwhite,IMG_ARC_PIE);
         }
-        
+    /**
+     * @param ? $img - использует изображение, которое вернул метод drawBg(); 
+     * данный метод выводит итоговое изображение
+     */    
     public function imgPrint($img){
         $colorwhite = imagecolorallocate($img, 0xFF, 0xFF, 0xFF);
         imageFill($img, 0, 0, $colorwhite);
@@ -33,6 +57,7 @@ class Chart {
         header('Content-type: image/png');
         imagepng($img);
         imagedestroy($img);
+
     }
 
 
